ffec6496ce800fae7ca7dc7f80d7568bb926ea1f
Verta-master <vertyagina@gmail.com>
2020-04-17 12:54:51 +0600

    Update 2020-04-17T06:54:45.763Z

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 //Mobile menu
-//$('.menu__btn').click(function() {
-//  $('.menu').toggleClass('menu--opened');
-//  $('.menu__list').slideToggle();
-//})
-//
-//if (window.innerWidth < 766) {
-//  $('.menu__link').click(function() {
-//    $('.menu__list').slideToggle();
-//    $('.menu').removeClass('menu--opened');
-//  })
-//}
+$('.menu__btn').click(function() {
+  $('.menu').toggleClass('menu--opened');
+})
 
 //Scroll to top
 $(window).scroll(function(){
